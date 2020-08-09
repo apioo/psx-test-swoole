bd6fcac10ae5da57e73479635f594555cd16ea7f
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-08-09 20:08:33 +0200

    update psx

--- routes.php
+++ routes.php
@@ -5,10 +5,8 @@ return [
     # API
     [['ANY'], '/population/popo', \PSX\Framework\App\Api\Population\CollectionPopo::class],
     [['ANY'], '/population/popo/:id', \PSX\Framework\App\Api\Population\EntityPopo::class],
-    [['ANY'], '/population/jsonschema', \PSX\Framework\App\Api\Population\CollectionJsonSchema::class],
-    [['ANY'], '/population/jsonschema/:id', \PSX\Framework\App\Api\Population\EntityJsonSchema::class],
-    [['ANY'], '/population/raml', \PSX\Framework\App\Api\Population\CollectionRaml::class],
-    [['ANY'], '/population/raml/:id', \PSX\Framework\App\Api\Population\EntityRaml::class],
+    [['ANY'], '/population/typeschema', \PSX\Framework\App\Api\Population\CollectionTypeSchema::class],
+    [['ANY'], '/population/typeschema/:id', \PSX\Framework\App\Api\Population\EntityTypeSchema::class],
     [['ANY'], '/population/openapi', \PSX\Framework\App\Api\Population\CollectionOpenAPI::class],
     [['ANY'], '/population/openapi/:id', \PSX\Framework\App\Api\Population\EntityOpenAPI::class],
     [['ANY'], '/population', \PSX\Framework\App\Api\Population\Collection::class],
@@ -21,8 +19,8 @@ return [
     [['GET'], '/tool/doc/:version/*path', \PSX\Framework\Controller\Tool\Documentation\DetailController::class],
     [['GET'], '/tool/routing', \PSX\Framework\Controller\Tool\RoutingController::class],
 
-    [['GET'], '/generator/raml/:version/*path', \PSX\Framework\Controller\Generator\RamlController::class],
-    [['GET'], '/generator/swagger/:version/*path', \PSX\Framework\Controller\Generator\SwaggerController::class],
     [['GET'], '/generator/openapi/:version/*path', \PSX\Framework\Controller\Generator\OpenAPIController::class],
+    [['GET'], '/generator/raml/:version/*path', \PSX\Framework\Controller\Generator\RamlController::class],
+    [['GET'], '/generator/typeschema/:version/*path', \PSX\Framework\Controller\Generator\TypeSchemaController::class],
 
 ];
