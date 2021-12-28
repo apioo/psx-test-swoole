6b2eb93aa5d4763ce87d80f0107eb204385c6058
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-12-28 17:26:48 +0100

    add github actions

--- routes.php
+++ routes.php
@@ -7,10 +7,6 @@ return [
     [['ANY'], '/population/popo/:id', \PSX\Framework\App\Api\Population\EntityPopo::class],
     [['ANY'], '/population/typeschema', \PSX\Framework\App\Api\Population\CollectionTypeSchema::class],
     [['ANY'], '/population/typeschema/:id', \PSX\Framework\App\Api\Population\EntityTypeSchema::class],
-    [['ANY'], '/population/openapi', \PSX\Framework\App\Api\Population\CollectionOpenAPI::class],
-    [['ANY'], '/population/openapi/:id', \PSX\Framework\App\Api\Population\EntityOpenAPI::class],
-    [['ANY'], '/population', \PSX\Framework\App\Api\Population\Collection::class],
-    [['ANY'], '/population/:id', \PSX\Framework\App\Api\Population\Entity::class],
 
     # tool controller
     [['GET'], '/tool', \PSX\Framework\Controller\Tool\DefaultController::class],
