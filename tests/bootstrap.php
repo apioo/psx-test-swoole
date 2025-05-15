a7203347c8cd3ca1c3dd71bf36419eccd3a7f3c4
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-05-15 21:55:19 +0200

    use global

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $container = require_once __DIR__ . '/../container.php';
 
 /** @var \PSX\Framework\Test\Environment $environment */
+global $environment;
 $environment = $container->get(\PSX\Framework\Test\Environment::class);
 $environment->setup();
